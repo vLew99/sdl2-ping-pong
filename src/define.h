ed0329e22793a61709fd48f0e3d7aa038ba31f9b
Vipul Chauhan <vip.chauhan99@gmail.com>
2023-03-17 18:24:28 +0530

    Added MACROS for score.c and score.h - required

--- src/define.h
+++ src/define.h
@@ -10,7 +10,6 @@
 #define WINDOW_FLAGS (SDL_WINDOW_OPENGL)
 #define RENDERER_FLADS (SDL_REDNERER_ACCELERATED)
 
-
 // player
 #define PLAYER_PADDLE_WIDTH 20
 #define PLAYER_PADDLE_HEIGHT 160
@@ -20,10 +19,16 @@
 #define BALL_SIZE 15
 #define BALL_SPEED 1.5
 
+// score text
+#define SCORE_FONT_SIZE 12
+#define SCORE_FONT_DPI 300
+#define SCORE_DIGITS "0123456789"
+#define SCORE_OFFSET_MIDDLE 100
+#define SCORE_POS_TOP 30
+
 // some functions
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) < (b) ? (a) : (b))
-#define abs(a) ((a) > 0 ? (a) : (-1*a))
-
+#define abs(a) ((a) > 0 ? (a) : (-1 * a))
 
 #endif
